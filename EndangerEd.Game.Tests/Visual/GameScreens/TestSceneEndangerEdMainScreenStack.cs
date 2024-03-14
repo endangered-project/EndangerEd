c0d0aa889e9866200812ee820e18fc7ece6a3761
Phawit Pornwattanakul <me@helloyeew.dev>
2024-03-14 21:44:48 +0700

    Add dummy scene for question

---
+++ EndangerEd.Game.Tests/Visual/GameScreens/TestSceneEndangerEdMainScreenStack.cs
@@ -0,0 +1,27 @@
+﻿using EndangerEd.Game.Objects;
+using EndangerEd.Game.Screens.Games;
+using osu.Framework.Screens;
+
+namespace EndangerEd.Game.Tests.Visual.GameScreens;
+
+public partial class TestSceneFourChoiceGameScreen : EndangerEdTestScene
+{
+    protected override void LoadComplete()
+    {
+        var gameScreen = new FourChoiceGameScreen(new Question()
+        {
+            QuestionText = "What is the question?",
+            Choices = ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+            Answer = "Choice 2",
+            ContentType = ContentType.Text
+        });
+        base.LoadComplete();
+        AddStep("add game screen", () =>
+        {
+            MainScreenStack.SwapScreenStack();
+            MainScreenStack.GameScreenStack.MainScreenStack.Push(gameScreen);
+        });
+        AddStep("clear game screen", gameScreen.Exit);
+
+    }
+}
--- EndangerEd.Game/Screens/Games/FourChoiceGameScreen.cs
+++ EndangerEd.Game/Screens/Games/FourChoiceGameScreen.cs
@@ -1,8 +1,52 @@
-﻿using EndangerEd.Game.Objects;
+﻿using EndangerEd.Game.Graphics;
+using EndangerEd.Game.Objects;
+using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
 
 namespace EndangerEd.Game.Screens.Games;
 
 public partial class FourChoiceGameScreen(Question question) : MicroGameScreen(question)
 {
-    
+    [BackgroundDependencyLoader]
+    private void load()
+    {
+        InternalChildren = new Drawable[]
+        {
+            new EndangerEdSpriteText()
+            {
+                Anchor = Anchor.TopCentre,
+                Origin = Anchor.TopCentre,
+                Text = CurrentQuestion.QuestionText,
+                Font = EndangerEdFont.GetFont(size: 40)
+            },
+            new FillFlowContainer()
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                AutoSizeAxes = Axes.Both,
+                Direction = FillDirection.Vertical,
+                Spacing = new osuTK.Vector2(0, 10),
+                Children = new Drawable[]
+                {
+                    new EndangerEdButton(CurrentQuestion.Choices[0])
+                    {
+                        Size = new osuTK.Vector2(200, 50)
+                    },
+                    new EndangerEdButton(CurrentQuestion.Choices[1])
+                    {
+                        Size = new osuTK.Vector2(200, 50)
+                    },
+                    new EndangerEdButton(CurrentQuestion.Choices[2])
+                    {
+                        Size = new osuTK.Vector2(200, 50)
+                    },
+                    new EndangerEdButton(CurrentQuestion.Choices[3])
+                    {
+                        Size = new osuTK.Vector2(200, 50)
+                    }
+                }
+            }
+        };
+    }
 }
