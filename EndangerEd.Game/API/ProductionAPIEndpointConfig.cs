d740443799aba5a3caceb3914b5dc53043c7a287
Phawit Pornwattanakul <me@helloyeew.dev>
2024-03-18 12:04:38 +0700

    Add production endpoint

--- EndangerEd.Game/API/ProductionAPIEndpointConfig.cs
+++ EndangerEd.Game/API/ProductionAPIEndpointConfig.cs
@@ -5,5 +5,7 @@ public class ProductionAPIEndpointConfig : APIEndpointConfig
     public ProductionAPIEndpointConfig()
     {
         APIBaseUrl = "https://endangered.helloyeew.dev/api/";
+        KnowledgeBaseUrl = "https://seed.helloyeew.dev/";
+        GameUrl = "https://endangered.helloyeew.dev/";
     }
 }
