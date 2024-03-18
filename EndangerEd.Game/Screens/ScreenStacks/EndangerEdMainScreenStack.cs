0cef7dbae68025868e69d4f12b8977daf9af058c
Phawit Pornwattanakul <me@helloyeew.dev>
2024-03-19 02:39:15 +0700

    Remove unused usage

--- EndangerEd.Game/Screens/ScreenStacks/EndangerEdMainScreenStack.cs
+++ EndangerEd.Game/Screens/ScreenStacks/EndangerEdMainScreenStack.cs
@@ -8,7 +8,6 @@ using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Graphics.Sprites;
-using osu.Framework.Logging;
 using osu.Framework.Screens;
 using osuTK;
 
