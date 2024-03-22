24b5d2b437deaf40a3a11a75575747dc3165a19d
Phawit Pornwattanakul <me@helloyeew.dev>
2024-03-23 02:49:47 +0700

    Finish cannon game

--- EndangerEd.Game/Screens/Games/CannonGameScreen.cs
+++ EndangerEd.Game/Screens/Games/CannonGameScreen.cs
@@ -37,10 +37,20 @@ public partial class CannonGameScreen(Question question) : MicroGameScreen(quest
     private APIRequestManager apiRequestManager { get; set; }
 
     private BindableBool answered = new BindableBool();
+    private bool allowFire = true;
     private EndangerEdButton endButton;
     private EndangerEdButton skipButton;
 
+    private Container boxContainer1;
+    private Container boxContainer2;
+    private Container boxContainer3;
+    private Container boxContainer4;
+
     private Box cannonBox;
+
+    private double angle;
+
+    private List<Circle> cannonBalls = new List<Circle>();
 
     [BackgroundDependencyLoader]
     private void load()
@@ -52,7 +62,15 @@ public partial class CannonGameScreen(Question question) : MicroGameScreen(quest
                 Anchor = Anchor.TopCentre,
                 Origin = Anchor.TopCentre,
                 Text = CurrentQuestion.QuestionText,
-                Font = EndangerEdFont.GetFont(size: 40)
+                Font = EndangerEdFont.GetFont(size: 40, weight: EndangerEdFont.FontWeight.Bold)
+            },
+            new EndangerEdSpriteText()
+            {
+                Anchor = Anchor.TopCentre,
+                Origin = Anchor.TopCentre,
+                Text = "Fire the cannon to the correct answer!",
+                Position = new Vector2(0, 50),
+                Font = EndangerEdFont.GetFont(size: 30)
             },
             endButton = new EndangerEdButton("End")
             {
@@ -107,18 +125,215 @@ public partial class CannonGameScreen(Question question) : MicroGameScreen(quest
             {
                 Anchor = Anchor.BottomCentre,
                 Origin = Anchor.BottomCentre,
-                Size = new Vector2(80,200),
-                Colour = Colour4.Red
+                Size = new Vector2(80, 100),
+                Colour = Colour4.Red,
+                Depth = -10
             }
         };
 
         if (question.ContentType == ContentType.Image)
         {
-
+            AddInternal(new FillFlowContainer()
+            {
+                Anchor = Anchor.TopCentre,
+                Origin = Anchor.TopCentre,
+                Position = new Vector2(0, 200),
+                Direction = FillDirection.Horizontal,
+                Spacing = new Vector2(100f),
+                Children = new Drawable[]
+                {
+                    boxContainer1 = new Container()
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(150, 150),
+                        Children = new Drawable[]
+                        {
+                            new Box()
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Red
+                            },
+                            new OnlineImageSprite(question.Choices[0])
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                RelativeSizeAxes = Axes.Both
+                            }
+                        }
+                    },
+                    boxContainer2 = new Container()
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(150, 150),
+                        Children = new Drawable[]
+                        {
+                            new Box()
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Red
+                            },
+                            new OnlineImageSprite(question.Choices[1])
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                RelativeSizeAxes = Axes.Both
+                            }
+                        }
+                    },
+                    boxContainer3 = new Container()
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(150, 150),
+                        Children = new Drawable[]
+                        {
+                            new Box()
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Red
+                            },
+                            new OnlineImageSprite(question.Choices[2])
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                RelativeSizeAxes = Axes.Both
+                            }
+                        }
+                    },
+                    boxContainer4 = new Container()
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(150, 150),
+                        Children = new Drawable[]
+                        {
+                            new Box()
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Red
+                            },
+                            new OnlineImageSprite(question.Choices[3])
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                RelativeSizeAxes = Axes.Both
+                            }
+                        }
+                    }
+                }
+            });
         }
         else
         {
-
+            AddInternal(new FillFlowContainer()
+            {
+                Anchor = Anchor.TopCentre,
+                Origin = Anchor.TopCentre,
+                Position = new Vector2(0, 200),
+                Direction = FillDirection.Horizontal,
+                Spacing = new Vector2(100f),
+                Children = new Drawable[]
+                {
+                    boxContainer1 = new Container()
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(150, 150),
+                        Children = new Drawable[]
+                        {
+                            new Box()
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Red
+                            },
+                            new SpriteText()
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Text = question.Choices[0]
+                            }
+                        }
+                    },
+                    boxContainer2 = new Container()
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(150, 150),
+                        Children = new Drawable[]
+                        {
+                            new Box()
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Red
+                            },
+                            new SpriteText()
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Text = question.Choices[1]
+                            }
+                        }
+                    },
+                    boxContainer3 = new Container()
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(150, 150),
+                        Children = new Drawable[]
+                        {
+                            new Box()
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Red
+                            },
+                            new SpriteText()
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Text = question.Choices[2]
+                            }
+                        }
+                    },
+                    boxContainer4 = new Container()
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Size = new Vector2(150, 150),
+                        Children = new Drawable[]
+                        {
+                            new Box()
+                            {
+                                Anchor = Anchor.TopCentre,
+                                Origin = Anchor.TopCentre,
+                                RelativeSizeAxes = Axes.Both,
+                                Colour = Colour4.Red
+                            },
+                            new SpriteText()
+                            {
+                                Anchor = Anchor.Centre,
+                                Origin = Anchor.Centre,
+                                Text = question.Choices[3]
+                            }
+                        }
+                    }
+                }
+            });
         }
 
         answered.BindValueChanged(answered =>
@@ -138,16 +353,89 @@ public partial class CannonGameScreen(Question question) : MicroGameScreen(quest
             IsOverTime = true;
             onChoiceSelected("");
         }
+
+        foreach (Circle cannonBall in cannonBalls)
+        {
+            // Check cannon collision
+            if (boxContainer1.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.TopLeft) || boxContainer1.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.TopRight) || boxContainer1.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.BottomLeft) || boxContainer1.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.BottomRight))
+            {
+                boxContainer1.FlashColour(Colour4.White, 500);
+                stopAllBullet();
+                allowFire = false;
+                onChoiceSelected(question.Choices[0]);
+            }
+            else if (boxContainer2.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.TopLeft) || boxContainer2.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.TopRight) || boxContainer2.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.BottomLeft) || boxContainer2.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.BottomRight))
+            {
+                boxContainer2.FlashColour(Colour4.White, 500);
+                stopAllBullet();
+                allowFire = false;
+                onChoiceSelected(question.Choices[1]);
+            }
+            else if (boxContainer3.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.TopLeft) || boxContainer3.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.TopRight) || boxContainer3.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.BottomLeft) || boxContainer3.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.BottomRight))
+            {
+                boxContainer3.FlashColour(Colour4.White, 500);
+                stopAllBullet();
+                allowFire = false;
+                onChoiceSelected(question.Choices[2]);
+            }
+            else if (boxContainer4.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.TopLeft) || boxContainer4.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.TopRight) || boxContainer4.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.BottomLeft) || boxContainer4.ScreenSpaceDrawQuad.Contains(cannonBall.ScreenSpaceDrawQuad.BottomRight))
+            {
+                boxContainer4.FlashColour(Colour4.White, 500);
+                stopAllBullet();
+                allowFire = false;
+                onChoiceSelected(question.Choices[3]);
+            }
+        }
+    }
+
+    private void stopAllBullet()
+    {
+        foreach (Circle cannonBall in cannonBalls)
+        {
+            cannonBall.ClearTransforms();
+        }
     }
 
     protected override bool OnMouseMove(MouseMoveEvent e)
     {
+        if (!allowFire)
+            return base.OnMouseMove(e);
+
         // Rotate the cannon using the mouse position
         // Compute the angle between the cannon and the mouse position
-        var angle = Math.Atan2(e.ScreenSpaceMousePosition.Y, e.ScreenSpaceMousePosition.X);
-        Logger.LogPrint($"Angle: {angle}");
+        // Angle will output between -1.5 to 0, normalize number to -60 to 60
+        angle = -Math.Atan2(e.MousePosition.Y - cannonBox.Position.Y, e.MousePosition.X - cannonBox.Position.X);
+        // Normalize the angle
+        angle = angle * 180 / Math.PI * 1.25 + 60;
+        Logger.Log($"Angle: {angle}");
         cannonBox.Rotation = (float)angle;
         return base.OnMouseMove(e);
+    }
+
+    protected override bool OnMouseDown(MouseDownEvent e)
+    {
+        if (!allowFire)
+            return base.OnMouseDown(e);
+
+        // Summon the cannon ball
+        Circle cannonBall = new Circle
+        {
+            Anchor = Anchor.BottomCentre,
+            Origin = Anchor.BottomCentre,
+            RelativePositionAxes = Axes.Both,
+            Size = new Vector2(30, 30),
+            Colour = Colour4.Yellow,
+            Position = cannonBox.Position
+        };
+        cannonBalls.Add(cannonBall);
+        AddInternal(cannonBall);
+        // Calculate target position using pythagorean theorem
+        // Lock y position to 1.3f
+        // Calculate x position using angle
+        double x = Math.Sin(angle * Math.PI / 180) * 1.3f;
+        Logger.Log($"X: {x}");
+        cannonBall.MoveTo(new Vector2((float)x, -1.5f), 1000, Easing.OutQuint);
+        return base.OnMouseDown(e);
     }
 
     protected override void LoadComplete()
